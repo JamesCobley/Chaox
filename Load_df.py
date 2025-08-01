fe1d8e4c9bce5de55343720ea274d57697322b6b
James Cobley <j_cobley@yahoo.com>
2025-08-01 16:43:11 +0100

    Update Load_df.py

--- Load_df.py
+++ Load_df.py
@@ -7,3 +7,6 @@ df = pd.read_excel('/content/aay7315_Data_file_S1.xlsx')
 # View column names and the first few rows
 print("Column names:\n", df.columns)
 df.head()
+
+# Check column data types
+df.dtypes
