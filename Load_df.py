dfd8b831ef03b5a7754051bfb5e710f7a642e42f
James Cobley <j_cobley@yahoo.com>
2025-08-01 16:42:28 +0100

    Create Load_df.py

---
+++ Load_df.py
@@ -0,0 +1,9 @@
+#Loads and inspects the source file in the colab environment
+import pandas as pd
+
+# Load the Excel file
+df = pd.read_excel('/content/aay7315_Data_file_S1.xlsx')
+
+# View column names and the first few rows
+print("Column names:\n", df.columns)
+df.head()
